2203338a5757702e6301e2c061421523314ae966
Oleksandra Dubrovina <sashaaagfg4@gmail.com>
2026-06-21 15:38:29 +0300

    fix

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,5 @@
 import css from './SearchBar.module.css';
+import toast from 'react-hot-toast';
 
 interface SearchBarProps {
   onSubmit: (query: string) => void;
@@ -9,6 +10,7 @@ export default function SearchBar({ onSubmit }: SearchBarProps) {
     const query = formData.get('query');
 
     if (typeof query !== 'string' || !query.trim()) {
+      toast.error('Please enter your search query.');
       return;
     }
 
